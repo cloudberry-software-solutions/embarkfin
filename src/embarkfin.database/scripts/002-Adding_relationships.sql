a05ec523347911b6a02ef454f8e94b17743b9a3a
Shawnie6 <rawjeeshawn86@gmail.com>
2019-04-02 21:59:55 +0200

    Updating the database scripts to add foreign keys to table asset.

---
+++ src/embarkfin.database/scripts/002-Adding_relationships.sql
@@ -0,0 +1,8 @@
+ALTER TABLE Asset
+ADD FOREIGN KEY ([Disposal_ID]) REFERENCES [Disposal](ID);
+
+ALTER TABLE Asset
+ADD FOREIGN KEY ([Location_ID]) REFERENCES [Location](ID);
+	
+ALTER TABLE Asset
+ADD FOREIGN KEY ([Asset_Type_ID]) REFERENCES [Asset_Type](ID);
