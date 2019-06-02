81393f92db0ddfef4fae48aee89f312738df6036
Shawn Rawjee <rawjeeshawn86@gmail.com>
2019-06-02 16:15:02 +0200

    informative comments

--- src/embarkfin-ui/src/app/services/asset-service.service.ts
+++ src/embarkfin-ui/src/app/services/asset-service.service.ts
@@ -8,14 +8,14 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 export class AssetServiceService {
 
   constructor(private http: HttpClient) { }
-
+// amend port according to your local machine
   calcUrl = 'https://localhost:5001/embarkfin/v1/assets/create';
 
   submit(asset: AssetModel) {
     // return this.http.post(this.calcUrl, '{serialNumber: ''testingGP'',Date_Purchased: ''2015-05-16T05:50:06 '',Period: ''5'',Purchase_Price: ''123000,12''}'');
 
     asset.Serial_Number = 'this test works';
-    
+
     return this.http.post(this.calcUrl, asset);
   }
 }
--- src/embarkfin.api/embarkfin.api/embarkfin.api/Startup.cs
+++ src/embarkfin.api/embarkfin.api/embarkfin.api/Startup.cs
@@ -31,6 +31,7 @@ namespace embarkfin.api
         {
             services.AddCors(o => o.AddPolicy("CorsPolicy", cp => cp.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod().AllowCredentials()));
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            //Amend connectionString according to your local machine
             services.AddDbContext<DatabaseContext>(options => options.UseSqlServer("Server=localhost; Database=Embarkfin; User Id = sa; Password= P@ssword1"));
             services.AddScoped<DatabaseContext>();
             services.AddScoped<AssetRepository>();
